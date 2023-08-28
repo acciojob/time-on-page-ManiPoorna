7335081464809f9698ebf74a67e43010f166987b
Mani Poorna <ndmprao2001@gmail.com>
2023-08-28 14:14:03 +0530

    V 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [time,setTime] = useState(0);
+
+  useEffect(()=>{
+    const interval = setInterval(() => {
+      setTime((prevState)=>prevState+1)
+    }, 1000);
+    return ()=>{
+      clearInterval(interval);
+    }
+  },[])
+  console.log(time)
+
   return (
     <div>
         {/* Do not remove the main div */}
+        You've been on this page for {time} seconds.
     </div>
   )
 }
